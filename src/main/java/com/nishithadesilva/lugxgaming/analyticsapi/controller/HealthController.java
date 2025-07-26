1d606f347c2e27c9b4f8397857f738a90244dc68
nishitha97 <nishithadesilva123@gmail.com>
2025-07-26 13:22:59 +0530

    minor code update

--- src/main/java/com/nishithadesilva/lugxgaming/analyticsapi/controller/HealthController.java
+++ src/main/java/com/nishithadesilva/lugxgaming/analyticsapi/controller/HealthController.java
@@ -1,34 +1,13 @@
 package com.nishithadesilva.lugxgaming.analyticsapi.controller;
 
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.PersistenceContext;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @RestController
 public class HealthController {
 
-    @PersistenceContext
-    private EntityManager entityManager;
-
-    @Value("${app.version:unknown}")
-    private String appVersion;
-
     @GetMapping("/health")
-    public Map<String, String> healthCheck() {
-        Map<String, String> response = new HashMap<>();
-        response.put("version", appVersion);
-        try {
-            entityManager.createNativeQuery("SELECT 1").getSingleResult();
-            response.put("status", "HEALTHY");
-        } catch (Exception e) {
-            response.put("status", "SICK");
-        }
-        return response;
+    public String healthCheck() {
+        return "Analytics Service is up and running";
     }
-
 }
